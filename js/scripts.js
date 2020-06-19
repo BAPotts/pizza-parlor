9922231971cfaa7b05f92299876ca606d0044019
Beverly Potts <pottsbeverly@gmail.com>
2020-06-19 12:52:05 -0400

    Add ui logic to take in topping selection as array, add toppings property to pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //Business Logic-----------
 
-function Pizza(size) {
+function Pizza(size, toppings) {
   this.size = size;
+  this.toppings = toppings;
 }
 
 
@@ -21,8 +22,12 @@ $(document).ready(function(){
 $("form").submit(function(event) {
   event.preventDefault();
   const size = $("input:radio[name=pizza-size]:checked").val();
+  const toppings=[];
+  $("input:checkbox[name=topping]:checked").each(function(){
+    toppings.push($(this).val());
   
-  let newPizza = new Pizza(size);
-  alert(newPizza.size);
+  }) 
+  let newPizza = new Pizza(size, toppings);
+  alert(newPizza.toppings[0]);
   })
 })
