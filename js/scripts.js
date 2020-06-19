98edcaafee1709351dd8dbc2e524ecdce3cd4631
Beverly Potts <pottsbeverly@gmail.com>
2020-06-19 12:02:41 -0400

    Add ui logic to call Pizza constructor and pass inputted size as argument

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,8 @@ $(document).ready(function(){
 $("form").submit(function(event) {
   event.preventDefault();
   const size = $("input:radio[name=pizza-size]:checked").val();
-  alert(size);
+  
+  let newPizza = new Pizza(size);
+  alert(newPizza.size);
   })
 })
